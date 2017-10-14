4496e3066567c35ab337d5bb99001e9bfd091009
ronrest <ronny.coding@gmail.com>
2017-10-14 16:43:13 +1100

    FEAT: Add load_image_and_seglabels()

--- prepare_data.py
+++ prepare_data.py
@@ -148,3 +148,70 @@ def rgb2seglabel(img, colormap, channels_axis=False):
     return label
 
 
+# ==============================================================================
+#                                                       LOAD_IMAGE_AND_SEGLABELS
+# ==============================================================================
+def load_image_and_seglabels(input_files, label_files, colormap, shape=(32,32), n_channels=3, label_chanel_axis=False):
+    """ Given a list of input image file paths and corresponding segmentation
+        label image files (with different RGB values representing different
+        classes), and a colormap list, it:
+
+        - loads up the images
+        - resizes them to a desired shape
+        - converts segmentation labels to single color channel image with
+          integer value of pixel representing the class id.
+
+    Args:
+        input_files:        (list of str) file paths for input images
+        label_files:        (list of str) file paths for label images
+        colormap:           (list or None) A list where each index represents the
+                            color value for the corresponding class id.
+                            Eg: for RGB labels, to map class_0 to black and
+                            class_1 to red:
+                                [(0,0,0), (255,0,0)]
+                            Set to None if images are already encoded as
+                            greyscale where the integer value represents the
+                            class id.
+        shape:              (2-tuple of ints) (width,height) to reshape images
+        n_channels:         (int) Number of chanels for input images
+        label_chanel_axis:  (bool)(default=False) Use color chanel axis for
+                            array of label images?
+    """
+    # Dummy proofing
+    assert n_channels in {1,3}, "Incorrect value for n_channels. Must be 1 or 3. Got {}".format(n_channels)
+
+    # Image dimensions
+    width, height = shape
+    n_samples = len(label_files)
+
+    # Initialize input and label batch arrays
+    X = np.zeros([n_samples, height, width, n_channels], dtype=np.uint8)
+    if label_chanel_axis:
+        Y = np.zeros([n_samples, height, width, 1], dtype=np.uint8)
+    else:
+        Y = np.zeros([n_samples, height, width], dtype=np.uint8)
+
+    for i in range(n_samples):
+        # Get filenames of input and label
+        img_file = input_files[i]
+        label_file = label_files[i]
+
+        # Resize input and label images
+        img = PIL.Image.open(img_file).resize(shape, resample=PIL.Image.CUBIC)
+        label_img = PIL.Image.open(label_file).resize(shape, resample=PIL.Image.NEAREST)
+
+        # Convert back to numpy arrays
+        img = np.asarray(img, dtype=np.uint8)
+        label_img = np.asarray(label_img, dtype=np.uint8)
+
+        # Convert label image from RGB to single value int class labels
+        if colormap is not None:
+            label_img = rgb2seglabel(label_img, colormap=colormap, channels_axis=label_chanel_axis)
+
+        # Add processed images to batch arrays
+        X[i] = img
+        Y[i] = label_img
+
+    return X, Y
+
+
