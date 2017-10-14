9d15a9ebd2222adc1cb02cb967d3fa3fc221f030
ronrest <ronny.coding@gmail.com>
2017-10-14 16:42:37 +1100

    FEAT: Add rgb2seglabel()

--- prepare_data.py
+++ prepare_data.py
@@ -123,3 +123,28 @@ def pixels_with_value(img, val):
     return np.all(img==np.array(val), axis=2)
 
 
+# ==============================================================================
+#                                                                   RGB2SEGLABEL
+# ==============================================================================
+def rgb2seglabel(img, colormap, channels_axis=False):
+    """ Given an RGB image stored as a numpy array, and a colormap that
+        maps from label id to an RGB color for that label, it returns a
+        new numpy array with color chanel size of 1 where the pixel
+        intensity values represent the class label id.
+
+    Args:
+        img:            (np array)
+        colormap:       (list) list of pixel values for each class label id
+        channels_axis:  (bool)(default=False) Should it return an array with a
+                        third (color channels) axis of size 1?
+    """
+    height, width, _ = img.shape
+    if channels_axis:
+        label = np.zeros([height, width,1], dtype=np.uint8)
+    else:
+        label = np.zeros([height, width], dtype=np.uint8)
+    for id in range(len(colormap)):
+        label[np.all(img==np.array(idcolormap[id]), axis=2)] = id
+    return label
+
+
