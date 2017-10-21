96be595c6dedc13e4944cecd7d279cff5c2dfcd7
ronrest <ronny.coding@gmail.com>
2017-10-22 01:52:21 +1100

    FIX: Pass batch_size to predictions function in train loop

--- model_base.py
+++ model_base.py
@@ -454,8 +454,8 @@ class ImageClassificationModel(object):
                     self.save_snapshot_in_session(sess, self.snapshot_file)
 
                     # Evaluate on full train and validation sets after each epoch
-                    train_acc, train_loss = self.evaluate_in_session(data["X_train"][:1024], data["Y_train"][:1024], sess)
-                    valid_acc, valid_loss = self.evaluate_in_session(data["X_valid"], data["Y_valid"], sess)
+                    train_acc, train_loss = self.evaluate_in_session(data["X_train"][:1024], data["Y_train"][:1024], sess, batch_size=batch_size)
+                    valid_acc, valid_loss = self.evaluate_in_session(data["X_valid"], data["Y_valid"], sess, batch_size=batch_size)
                     self.update_evals_dict(train_acc=train_acc, train_loss=train_loss, valid_acc=valid_acc, valid_loss=valid_loss)
                     self.save_evals_dict()
 
@@ -821,8 +821,8 @@ class SegmentationModel(ImageClassificationModel):
                     self.save_snapshot_in_session(sess, self.snapshot_file)
 
                     # Evaluate on full train and validation sets after each epoch
-                    train_iou, train_loss = self.evaluate_in_session(data["X_train"][:1000], data["Y_train"][:1000], sess)
-                    valid_iou, valid_loss = self.evaluate_in_session(data["X_valid"], data["Y_valid"], sess)
+                    train_iou, train_loss = self.evaluate_in_session(data["X_train"][:1000], data["Y_train"][:1000], sess, batch_size=batch_size)
+                    valid_iou, valid_loss = self.evaluate_in_session(data["X_valid"], data["Y_valid"], sess, batch_size=batch_size)
                     self.update_evals_dict(train_iou=train_iou, train_loss=train_loss, valid_iou=valid_iou, valid_loss=valid_loss)
                     self.save_evals_dict()
 
