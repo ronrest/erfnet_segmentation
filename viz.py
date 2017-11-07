ba0d2a314c3d046b2e766d7886e24ce8e35b4309
ronrest <ronny.coding@gmail.com>
2017-11-07 21:42:47 +1100

    FEAT: batch2grid now should handle images without color chanels axis

--- viz.py
+++ viz.py
@@ -11,7 +11,6 @@ import PIL.Image
 import PIL.ImageChops
 
 
-
 __author__ = "Ronny Restrepo"
 __copyright__ = "Copyright 2017, Ronny Restrepo"
 __credits__ = ["Ronny Restrepo"]
@@ -50,10 +49,11 @@ def train_curves(train, valid, saveto=None, title="Accuracy over time", ylab="ac
         plt.close()
 
 
+
 # ==============================================================================
 #                                                                   BATCH 2 GRID
 # ==============================================================================
-def batch2grid(X, rows, cols):
+def batch2grid(imgs, rows, cols):
     """
     Given a batch of images stored as a numpy array of shape:
 
@@ -79,26 +79,36 @@ def batch2grid(X, rows, cols):
     # TODO: have a resize option to rescale the individual sample images
     # TODO: Have a random shuffle option
     # TODO: Set the random seed if needed
-    # TODO: Let it accept images without color channels axis
-    assert (X.ndim == 4), "X is wrong dimensions. Reecived {}".format(X.shape)
     assert rows>0 and cols>0, "rows and cols must be positive integers"
 
     # Prepare dimensions of the grid
     n_cells = (rows*cols)
-    n_samples, img_height, img_width, n_channels = X.shape
+    imgs = imgs[:n_cells] # Only use the number of images needed to fill grid
+    n_samples, img_height, img_width, n_channels = imgs.shape
+
+    # Image dimensions
+    n_dims = imgs.ndim
+    assert n_dims==3 or n_dims==4, "Incorrect # of dimensions for input array"
+
+    # Deal with images that have no color channel
+    if n_dims == 3:
+        imgs = np.expand_dims(imgs, axis=3)
 
     # Handle case where there is not enough images in batch to fill grid
     if n_cells > n_samples:
         n_gap = n_cells - n_samples
-        grid = np.pad(X, pad_width=[(0,n_gap),(0,0), (0,0), (0,0)], mode="constant", constant_values=0)
-    else:
-        grid = X[:n_cells]
+        imgs = np.pad(imgs, pad_width=[(0,n_gap),(0,0), (0,0), (0,0)], mode="constant", constant_values=0)
 
     # Reshape into grid
-    grid = grid.reshape(rows,cols,img_height,img_width,n_channels).swapaxes(1,2)
+    grid = imgs.reshape(rows, cols,img_height,img_width,n_channels).swapaxes(1,2)
     grid = grid.reshape(rows*img_height,cols*img_width,n_channels)
 
+    # If input was flat images with no color channels, then flatten the output
+    if n_dims == 3:
+        grid = grid.squeeze(axis=2) # axis 2 because batch dim has been removed
+
     return grid
+
 
 
 # ==============================================================================
