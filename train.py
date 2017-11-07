0e9233c99a2ef202cd769f69e73ddd1248de4eb5
ronrest <ronny.coding@gmail.com>
2017-11-07 22:31:36 +1100

    FEAT: train on erfnet

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from data_processing import prepare_data, calculate_class_weights
 from model_base import SegmentationModel
 
+from erfnet import erfnetA
 
 __author__ = "Ronny Restrepo"
 __copyright__ = "Copyright 2017, Ronny Restrepo"
@@ -122,10 +123,19 @@ if __name__ == '__main__':
     n_valid = 128
     data_file = "data_256.pickle"
     vgg16_snapshot = "/path/to/vgg16/vgg_16.ckpt"
+
+    # PREPARE DATA
     DATA_LIMIT = None
     data = prepare_data(data_file, valid_from_train=True, n_valid=n_valid, max_data=DATA_LIMIT)
     n_classes = len(data["id2label"])
+
+    # CLASS WEIGHTS
     class_weights = calculate_class_weights(data["Y_train"], n_classes=n_classes, method="paszke", c=1.10)
     # class_weights = calculate_class_weights(data["Y_train"], n_classes=n_classes, method="logeigen2")
 
+
+    model = SegmentationModel("erfnetA_01", img_shape=[256,256], n_classes=len(data["id2label"]))
+    model.set_class_weights(class_weights)
+    model.create_graph_from_logits_func(erfnetA)
+    model.train(data, n_epochs=10, print_every=1, batch_size=8, alpha=5e-4, dropout=0.3, aug_func=None, viz_every=1)
     print("DONE!!!")
