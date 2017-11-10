9ba3c6c93dcaef45cecc25d8cb4588c3d93d6519
ronrest <ronny.coding@gmail.com>
2017-11-10 23:55:58 +1100

    FEAT: Version of erfnet with l2 regularization support

--- erfnet.py
+++ erfnet.py
@@ -109,3 +109,45 @@ def erfnetA(X, Y, n_classes, alpha=0.001, dropout=0.3, l2=None, is_training=Fals
 
     x = upsample(x, n_filters=n_classes, is_training=is_training, name="up23")
     return x
+def erfnetB(X, Y, n_classes, alpha=0.001, dropout=0.3, l2=None, is_training=False):
+    """
+    Uses L2 regularization.
+    """
+    print("DEBUG: L2 passed on to ERFNETB{}".format(l2))
+    # TODO: Use TF repeat for some of these repeating layers
+    # TODO: register factorized_res_module and upsample and downsample with arg_scope
+    #       and pass dropout, is_training, etc to it.
+    # TODO: Add weight decay.
+    with tf.name_scope("preprocess") as scope:
+        x = tf.div(X, 255., name="rescaled_inputs")
+
+    x = downsample(x, n_filters=16, is_training=is_training, l2=l2, name="d1")
+
+    x = downsample(x, n_filters=64, is_training=is_training, l2=l2, name="d2")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres3")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres4")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres5")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres6")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres7")
+
+    # TODO: Use dilated convolutions
+    x = downsample(x, n_filters=128, is_training=is_training, l2=l2, name="d8")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 2], l2=l2, name="fres9")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 4], l2=l2, name="fres10")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 8], l2=l2, name="fres11")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 16], l2=l2, name="fres12")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 2], l2=l2, name="fres13")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 4], l2=l2, name="fres14")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 8], l2=l2, name="fres15")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 16], l2=l2, name="fres16")
+
+    x = upsample(x, n_filters=64, is_training=is_training, l2=l2, name="up17")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres18")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres19")
+
+    x = upsample(x, n_filters=16, is_training=is_training, l2=l2, name="up20")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres21")
+    x = factorized_res_module(x, is_training=is_training, dilation=[1, 1], l2=l2, name="fres22")
+
+    x = upsample(x, n_filters=n_classes, is_training=is_training, l2=l2, name="up23")
+    return x
