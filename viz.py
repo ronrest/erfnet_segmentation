b26c66181d7b0b0e13e422350facf7cf0198dba8
ronrest <ronny.coding@gmail.com>
2017-12-27 18:32:57 +1100

    FIX: viz_sample_augmentations now configured to deal with semantic segmentation data. - Also changed name to viz_sample_segmentation_augmentations()

--- viz.py
+++ viz.py
@@ -191,7 +191,7 @@ def array2pil(x):
 # ==============================================================================
 #                                                       VIZ_SAMPLE_AUGMENTATIONS
 # ==============================================================================
-def viz_sample_augmentations(X, aug_func, n_images=5, n_per_image=5, saveto=None):
+def viz_sample_segmentation_augmentations(X, Y, aug_func, colormap, n_images=5, n_per_image=5, saveto=None):
     """ Given a batch of data X, and Y,  it takes n_images samples, and performs
         `n_per_image` random transformations for segmentation data on each of
         those images. It then puts them in a grid to visualize. Grid size is:
@@ -199,11 +199,18 @@ def viz_sample_augmentations(X, aug_func, n_images=5, n_per_image=5, saveto=None
 
     Args:
         X:          (np array) batch of images
+        Y:          (np array) batch of labels
         aug_func:   (func) function with API `aug_func(X, Y)` that performs
                     random transformations on the images for segmentation
                     purposes.
-        n_images:   (int)
-        n_per_image:(int)
+        colormap:   (list of 3-tuples of ints)
+                    A list where each index represents the RGB value
+                    for the corresponding class id.
+                    Eg: to map class_0 to black and class_1 to red:
+                        [(0,0,0), (255,0,0)]
+
+        n_images:   (int) Number of base images to use for visualization
+        n_per_image:(int) Number of augmentations to show per base image
         saveto:     (str or None)
 
     Returns: (None, or PIL image)
@@ -215,16 +222,20 @@ def viz_sample_augmentations(X, aug_func, n_images=5, n_per_image=5, saveto=None
         samples.show()
     """
     X = X[:n_images]
+    Y = Y[:n_images]
     grid = []
+    colormap = np.array(colormap).astype(np.uint8)
 
     # Perform Augmentations
     for col in range(n_per_image):
-        x = aug_func(X)
-        grid.append(x)
+        x,y = aug_func(X, Y)
+        y = colormap[y]
+        z = np.append(x, y, axis=2) # append the label to the right
+        grid.append(z)
 
     # Put into a grid
     _, height, width, n_channels = X.shape
-    grid = np.array(grid, dtype=np.uint8).reshape(n_images*n_per_image, height, width, n_channels)
+    grid = np.array(grid, dtype=np.uint8).reshape(n_images*n_per_image, height, 2*width, n_channels)
     grid = batch2grid(grid, n_per_image, n_images)
 
     # Convert to PIL image
